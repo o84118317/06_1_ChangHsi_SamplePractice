70e2962b6c19bd4b173ede14a0860f288a4d7150
Chang hsi <o84118317@gmail.com>
2018-08-28 16:46:14 +0800

    array

---
+++ array.js
@@ -0,0 +1,17 @@
+let fruits = ['Apple', 'Orange', 'Plum'];
+
+alert ( fruits[0] ); // Apple
+alert ( fruits[1] ); // Orange
+alert ( fruits[2] ); // Plum
+
+fruits[2] = 'Pear';
+// now ['Apple', 'Orange', 'Pear']
+// We can replace an element
+
+fruits[3] = 'Lemon';
+// now ['Apple', 'Orange', 'Pear', 'Lemon']
+// add a new one to the array
+
+alert( fruits.length ); 
+// 4
+// The total count of the elements in the array is its length
