77f8da3d2689931a31be61515e65f899e18d1e42
Chang hsi <o84118317@gmail.com>
2018-08-28 15:44:39 +0800

    new Ocject Code Practice

---
+++ newPractice.js
@@ -0,0 +1,10 @@
+function User(name) {
+	if (!new.target) { // if you run me without
+		return new User(name); // ... I will add new for you 
+	}
+
+	this.name = name;
+}
+
+let jonh = User('John'); // redirects call to new User
+alert (john.name); // John 
