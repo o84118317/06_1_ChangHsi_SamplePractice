3a5355b85289f8e729402fe1b2613203ac9e5415
Chang hsi <o84118317@gmail.com>
2018-08-29 15:33:24 +0800

    Push and Pop

--- pushpop.js
+++ pushpop.js
@@ -1,4 +1,9 @@
 let fruits = ['Apple', 'Orange', 'Pear'];
+
 alert ( fruits.pop() ); // remove 'Pear' and alert it
 
 alert ( fruits );  // Apple, Orange
+
+fruits.push('Pear');
+
+alert( fruits ); // Apple, Orange, Pear
