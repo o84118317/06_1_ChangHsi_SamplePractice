fba2ff35116cabe9ea662dc5c8d6e7b8fb98041c
Chang hsi <o84118317@gmail.com>
2018-08-29 15:38:37 +0800

    Shift and Unshift

---
+++ shiftAndUnshift.js
@@ -0,0 +1,9 @@
+let fruits = ['Apple', 'Orange', 'Pear'];
+
+alert ( fruits.shift() ); // remove Apple and alert it
+alert ( fruits ); // Orange, Pear
+
+fruits.unshift('Apple');
+alert ( fruits ); // Apple, Orange, Pear
+
+// Methods push and unshift  can add multiple elements at once:
