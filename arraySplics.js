34249abc7fa6c47274867cb552e3415da8908d3c
Chang hsi <o84118317@gmail.com>
2018-08-29 16:17:59 +0800

    splice returns the aray of removed elements

--- arraySplics.js
+++ arraySplics.js
@@ -7,6 +7,13 @@ alert( arr ); //['I', 'JavaScript']
 // remove 3 elements and replace them by the other two
 
 let arr = ['I', 'study', 'JavaScript', 'right', 'now'];
-arr.splice( 0, 3, 'Let\'s','dance');
-// remove 3 first elements and replace them by another 
+arr.splice( 0, 3, 'Let\'s','dance');// remove 3 first elements and replace them by another 
 alert( arr ); // now['let's', 'dance', 'right', 'now']
+
+
+// splice returns the array of removed elements
+
+let arr = ['I', 'study', 'JavaScript', 'right', 'now'];
+let removed = arr.splice(0, 2); // remove 2 first elements and store in removed
+alert ( removed ); // 'I',' study' -> array od removed elements
+
