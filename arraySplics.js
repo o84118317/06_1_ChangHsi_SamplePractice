f2661edc94f28c1928b086c36897fa6e94cc2221
Chang hsi <o84118317@gmail.com>
2018-08-29 16:14:53 +0800

    remove elements and replace then by the other

--- arraySplics.js
+++ arraySplics.js
@@ -3,3 +3,10 @@
 let arr = ['I', 'study', 'JavaScript'];
 arr.splice(1,1); // from index 1 remove 1 element
 alert( arr ); //['I', 'JavaScript']
+
+// remove 3 elements and replace them by the other two
+
+let arr = ['I', 'study', 'JavaScript', 'right', 'now'];
+arr.splice( 0, 3, 'Let\'s','dance');
+// remove 3 first elements and replace them by another 
+alert( arr ); // now['let's', 'dance', 'right', 'now']
