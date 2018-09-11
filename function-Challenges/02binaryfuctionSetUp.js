2b796dbfd62b1b9786ade5f671ae3b5cc04475ba
Chang hsi <o84118317@gmail.com>
2018-09-11 17:22:43 +0800

    rename and challenges 02

---
+++ function-Challenges/02binaryfuctionSetUp.js
@@ -0,0 +1,28 @@
+/*
+Write three binary functions,
+add, sub, and mul, that take 
+two numbers and return their
+sum, difference, and product
+
+add(3,4)
+sub(3,4)
+mul(3,4)
+*/
+
+function add(a,b) {
+	return a + b;
+}
+
+function sub(a,b) {
+	return a - b;
+}
+
+function mul(a,b) {
+	return a * b;
+}
+
+
+
+add(3,4);
+sub(3,4);
+mul(3,4);
