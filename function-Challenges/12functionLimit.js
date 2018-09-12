e765f19528bc31c0f68bf7f6f7e513fa3771d29c
Chang hsi <o84118317@gmail.com>
2018-09-12 21:01:20 +0800

    function chellanges 12 function Limit

---
+++ function-Challenges/12functionLimit.js
@@ -0,0 +1,34 @@
+/*
+Write a limit function that 
+allows a binary function to be 
+called a limited number of times.
+
+let add_ltd =limit(add, 1);
+add_ltd(3, 4); //7
+add_ltd(3, 5); // undefined
+
+*/
+
+
+// my answer
+function limit(binary,timer) {
+	return function (a, b) {
+		let setTime = timer;
+		timer -= 1;
+		if (setTime) {
+			return binary(a, b);
+		};
+	};
+}
+
+// correct answer
+
+function limit(binary, count) {
+	return function (a, b) {
+		if (count >= 1) {
+			count -= 1;
+			return binary(a, b);
+		}
+		return undefined;
+	};
+}
