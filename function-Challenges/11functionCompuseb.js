1a0d8cb9f8ab7a23012c11a82293b5eb2114516d
Chang hsi <o84118317@gmail.com>
2018-09-12 20:36:39 +0800

    function challenges 11 function compuseb correct answer

--- function-Challenges/11functionCompuseb.js
+++ function-Challenges/11functionCompuseb.js
@@ -7,3 +7,11 @@ calls them both
 composeb(add, mul) (2, 3, 7)  // 35
 
 */
+
+function composeb(one, two) {
+	return function (a, b, c) {
+		//WRONG! return one(a, b)two(c);
+		// the correct answer is
+		return	two( one(a, b), c);
+	};
+}
