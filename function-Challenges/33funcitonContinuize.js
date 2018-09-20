22c5a70603016f0303d114a24cf201d0123b2428
Chang hsi <o84118317@gmail.com>
2018-09-20 20:36:26 +0800

    function challenges 33 function Continuizw question set

---
+++ function-Challenges/33funcitonContinuize.js
@@ -0,0 +1,9 @@
+/*
+Make a function continuize
+that takes a unary function,
+and returns a function that
+takes a callback and an argument.
+
+sqrtc = continuize(Math.sqrt);
+sqrtc(alert, 81) // 9
+*/
