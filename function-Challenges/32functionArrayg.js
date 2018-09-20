120b0edb509acff819a69ceac2b17d712b009d7f
Chang hsi <o84118317@gmail.com>
2018-09-20 20:31:38 +0800

    function challenges 32 function Arrayg correct answer

--- function-Challenges/32functionArrayg.js
+++ function-Challenges/32functionArrayg.js
@@ -8,11 +8,24 @@ arrayg(3)() // [3]
 arrayg(3)(4)(5)() // [3, 4, 5]
 */
 
-
+//  my answer
 function arrayg(first) {
 	if (first === undefined) {
 		return array.push[undefined];
 	}
 	array.push[first];
 	return arrayg;
+}
+
+// correct answer
+function liftg(first) {
+	let array = [];
+	function more(next) {
+		if (next === undefined) {
+			return array;
+		}
+		array.push(next);
+		return more;
+	}
+	return more(first);
 }
