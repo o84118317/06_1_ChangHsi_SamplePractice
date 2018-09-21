c863c6a45529b3de52645fa8d7c890b439694ad5
Chang hsi <o84118317@gmail.com>
2018-09-21 14:26:54 +0800

    function challenges 34 function Array Wrapper question setting

---
+++ function-Challenges/34funcrionArrayWrapper.js
@@ -0,0 +1,13 @@
+/*
+Make an array wrapper object
+with methods get, store, and
+append, such that an attacter
+cannot get access to the
+provate array.
+
+myvector = vector();
+myvector.append(7);
+myvector.store(1,8);
+myvector.get(0); // 7
+myvector.get(1); // 8
+*/
